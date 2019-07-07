cf24f5eabbb7dd55e25ac82eb86fee1071cbe0b9
Sugma-Lato <52589736+Sugma-Lato@users.noreply.github.com>
2019-07-07 23:53:10 +0700

    update tambah dikit

--- projek.cpp
+++ projek.cpp
@@ -1,24 +1,22 @@
 #include<iostream> //Library standart input output
-#include<iomanip> //Library utnuk memanggil fungsi setfill, setw, setbase
 using namespace std;
  
 
 int main()
 {
 
-char const* character[] = {"", "", "", "", "", "", "", "",
-                            "\\a","\\b","\\t","\\n","\\v","\\f","\\r", "",
-                            "", "", "", "", "", "", "", "",
-                            "", "", "", "", "", "", "", ""};
  
 //Deklarasi variable
-char c, karakter;
-int row, pilih;
+char karakter; //Untuk case 1/ASCII Tabel
+char karakter2[10]; //Kita batasi input hanya 10 karakter
+
+int pilih;
 
 //Print baner biar bagus
 printf(" | *************************************|");cout<<endl;
 printf(" | 1. Print ASCII Tabel\t\t\t|");cout<<endl;
-printf(" | 2. Konversi ASCII ke Hex, Dec, Oct\t|");cout<<endl;
+printf(" | 2. Konversi Char[1] ke Hex,Dec,Oct\t|");cout<<endl;
+printf(" | 3. Konversi Char[10] ke Hex,Dec,Oct\t|");cout<<endl;
 printf(" | *************************************|");
 
 //Meminta input untuk memilih tool
@@ -30,51 +28,59 @@ cout << "\n";
 
 //Inputan dari 'pilih' diatas kita lempar ke Kondisi 'switch' untuk memilih 2 tool
 switch(pilih)
-   {
+{
+   
    
    // 1. Tabel ASCII
    case 1:
-      { 
-
- cout << "ASCII Tabel" << endl;
-      for(int i = 0; i < 16; i++)
+    { 
+     cout << "ASCII Tabel\n";
+     cout << "-----------\n";
+     
+     for(int i=1; i<255; i++)
       {
-        row = i;
-        while (row <= 127) {
-            if (row < 32)
-                cout << setfill('0') << setw(2) << setbase(16)
-                     << row << " = " << setw(3) << setfill(' ')
-                     << character[i] << " | ";
-            
-            else if (row >= 32 && row < 127)
-            {
-                c = row;
-                cout << setfill('0') << setw(2) << setbase(16)
-                     << row << " = " << setw(3) << setfill(' ')
-                     << c << " | ";
-            }
-            
-            else
-                cout << setfill('0') << setw(2) << setbase(16)
-                     << row << " = " << setw(3) << setfill(' ')
-                     << "DEL" << " | ";
-            row = row + 16;
-         }
-         cout << endl;
-       }
-      break;
+      karakter = i;
+      cout << i << "=" << karakter << "\t |" ;
       }
+      break;
+    }
     
-   // 2. Konversi karaKter ASCII ke OCT, DEC, HEX
+   
+   // 2. Konversi 1 karaKter ke OCT, DEC, HEX
    case 2:
+    {
       cout << "Masukkan karakter : ";
-      cin >> karakter;
-      cout << "ASCII\t:" << karakter << endl          // output ASCII
+      cin >> karakter2;
+
+      cout 
        << "OCT\t:" << oct << (int)karakter << endl   // output Octal
        << "DEC\t:" << dec << (int)karakter << endl   // output Decimal
        << "HEX\t:" << hex << (int)karakter << endl;  // output Hexadecimal
-      break;
 
-   }
+    }
+
+
+    // 3. Konversi 10 karaKter ke OCT, DEC, HEX
+    case 3:
+    {
+
+      cout << "Masukkan karakter : ";
+      cin >> karakter2;
+
+      cout << "\nHasil konversi untuk 10 karakter [Kita sengaja batasi 10 karakter]\n";
+      cout << "HEX\t:";
+      for(int i=0; i<= 10; ++i)
+      cout << hex << (int)karakter2[i];              // output Hexadecimal
+ 
+      cout << "\nDEC\t:";
+      for(int i=0; i<= 10; ++i)
+      cout << dec << (int)karakter2[i];              // output Decimal
+
+      cout << "\nOCT\t:";
+      for(int i=0; i<= 10; ++i)
+      cout << oct << (int)karakter2[i];              // output Octal
+    }
+
+}
 } 
 
