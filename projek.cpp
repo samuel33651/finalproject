5569cdff3f9796d96928752238079843645fc42d
samuel33651 <52580687+samuel33651@users.noreply.github.com>
2019-07-10 20:02:16 +0700

    Tambah Fitur + Rapiin Tabel ASCII

--- projek.cpp
+++ projek.cpp
@@ -1,5 +1,6 @@
 #include<iostream> //Library standart input output
 #include <conio.h>
+#include<iomanip> //Library utnuk memanggil fungsi setfill, setw, setbase
 
 using namespace std;
  
@@ -9,18 +10,17 @@ int main()
 
  
 //Deklarasi variable
+int angka;
 char karakter, pilih; //Untuk case 1/ASCII Tabel
 char karakter2[10]; //Kita batasi input hanya 10 karakter
 char karakter3;
 
 //Print baner biar bagus
-//Untuk deteksi tombol keyboard kita baru mampu deteksi ENTER, SHIFT, TAB, CAPS LOCK dengan fungsi getchar()
-  //sedangkan untuk tombol yg lain kita belum mampu menemukan caranya
-
 printf(" | *************************************|");cout<<endl;
 printf(" | 1. Print ASCII Tabel\t\t\t|");cout<<endl;
 printf(" | 2. Konversi Char[1] ke Hex,Dec,Oct\t|");cout<<endl;
 printf(" | 3. Konversi Char[10] ke Hex,Dec,Oct\t|");cout<<endl;
+printf(" | 4. Konversi Desimal ke Binary\t|");cout<<endl;
 printf(" | *************************************|");
 
 //Meminta input untuk memilih tool
@@ -40,15 +40,18 @@ switch(pilih)
    // 1. Tabel ASCII
    case '1':
     { 
-     cout << "ASCII Tabel\n";
-     cout << "-----------\n";
-     
-     for(int i=1; i<255; i++)
+     cout << "ASCII Tabel | Dimulai dari 30 ==> 255\n"; //Saya mulai dari ke 30 karena 1-30 merupakan karakter non-printable
+     cout << "--------------------------------------------------------------------------\n";
+      
+      //looping sampai karakter ke 255
+      for (int i=30;i<=255;i++)
       {
-      karakter = i;
-      cout << i << "=" << karakter << "\t |" ;
+        karakter=i;
+        cout << setw(2) << karakter 
+             << setw(6) << i;
       }
-      break;
+     cout << "\n--------------------------------------------------------------------------\n";
+     break;
     }
     
    
@@ -82,6 +85,27 @@ switch(pilih)
       cout << "\nOCT\t:";
       for(int i=0; i<= 10; ++i)
       cout << oct << (int)karakter2[i];              // output Octal
+
+
+    // 4. Konversi 1 karaKter ke Binary
+    case '4':      
+      cout << "Masukkan angka = "; cin >> angka;
+
+      int MaksimalPanjang[32], i=0; 
+      
+      while (angka > 0) 
+        { 
+        //Increament looping : Hasil disimpan di MaksimalPanjang
+        MaksimalPanjang[i] = angka % 2; 
+        angka /= 2; 
+        i++; 
+        } 
+
+      //Menampilkan angka yg diinput (dari step diatas) ke binary
+      for (int a = i-1; a >= 0; a--) 
+      cout << MaksimalPanjang[a]; 
+
+      break;
 
 }
 } 
